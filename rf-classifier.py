506c3a5935e8216b49ba29bd734d5b5f000e2927
Andrew Luo <jihanjie.luo@gmail.com>
2025-06-24 22:29:07 -0400

    Add timer

--- rf-classifier.py
+++ rf-classifier.py
@@ -10,6 +10,7 @@ from sklearn.metrics import fbeta_score
 from dataloader import load_unsw_nb15
 
 import numpy as np
+import time
 
 # 249050
 
@@ -23,10 +24,14 @@ csvs = [
 csvs = [DATA_ROOT + filename for filename in csvs]
 
 def classify(frac = 0.8, size = 700000, randomize = False):
+    t = time.monotonic()
     print(f"Starting test for size {size}.")
     X_train, X_test, y_train, y_test = load_unsw_nb15(csvs, train_frac=frac, randomize=randomize, max_rows=size)
+    print(f"Data loaded in time {time.monotonic() - t}")
+    t = time.monotonic()
     rf = RandomForestClassifier()
     rf.fit(X_train, y_train)
+    print(f"Training completed time {time.monotonic() - t}")
 
     y_pred = rf.predict(X_test)
     f2_score = fbeta_score(y_test, y_pred, beta=2, average="binary")
